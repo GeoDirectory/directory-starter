3a64cbe4eaafaa1f8479238ccbc87ccd1ad2e186
Giri <giridharanbtech@gmail.com>
2015-07-31 21:34:16 +0530

    Some settings and designs added

--- header.php
+++ header.php
@@ -9,3 +9,26 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<header id="site-header" class="site-header" role="banner">
+	<div class="container">
+		<div class="site-logo-wrap">
+			<?php if ( get_theme_mod( 'directory_theme_logo' ) ) : ?>
+				<div class='site-logo'>
+					<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'directory_theme_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
+				</div>
+			<?php else : ?>
+				<hgroup>
+					<h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
+				</hgroup>
+			<?php endif; ?>
+		</div>
+		<nav id="primary-nav" class="primary-nav" role="navigation">
+			<?php wp_nav_menu(array(
+				'container' => false,
+				'theme_location' => 'primary-menu',
+			)); ?>
+		</nav>
+	</div>
+</header>
--- lib/scripts.php
+++ lib/scripts.php
@@ -13,5 +13,10 @@ function directory_theme_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	$query_args = array(
+		'family' => 'Lato:400,700',
+	);
+	wp_enqueue_style( 'directory-theme_google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'directory_theme_scripts' );
