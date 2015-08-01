bce870f90c1880801b4f255e4c93b17a4616edb2
Giri <giridharanbtech@gmail.com>
2015-08-01 21:50:23 +0530

    some styles added

---
+++ lib/utils.php
@@ -0,0 +1,20 @@
+<?php
+function directory_theme_paginate() {
+	global $wp_query;
+	$big_num = 999999999;
+	if ( $wp_query->max_num_pages <= 1 )
+		return;
+	echo '<nav class="pagination">';
+	echo paginate_links( array(
+		'base'         => str_replace( $big_num, '%#%', esc_url( get_pagenum_link($big_num) ) ),
+		'format'       => '',
+		'current'      => max( 1, get_query_var('paged') ),
+		'total'        => $wp_query->max_num_pages,
+		'prev_text'    => '&larr;',
+		'next_text'    => '&rarr;',
+		'type'         => 'list',
+		'end_size'     => 3,
+		'mid_size'     => 3
+	) );
+	echo '</nav>';
+}
