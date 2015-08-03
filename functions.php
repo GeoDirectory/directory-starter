e973a778b3528f10b4b621617dd8ad1eb788bd0c
Giri <giridharanbtech@gmail.com>
2015-08-03 21:48:55 +0530

    some styles and settings file added

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@ To manage your codebase properly, you should place all your functions in 'lib' f
 Include each function file here.
 */
 
+require_once get_template_directory() . '/lib/utils.php';             // Utility functions goes here
 require_once get_template_directory() . '/lib/config.php';            // Configuration goes here
 require_once get_template_directory() . '/lib/init.php';              // Theme setup functions goes here
 require_once get_template_directory() . '/lib/cleanup.php';           // Cleanup functions goes here
 require_once get_template_directory() . '/lib/scripts.php';           // CSS and JS includes goes here
-require_once get_template_directory() . '/lib/utils.php';             // Utility functions goes here
 require_once get_template_directory() . '/lib/nav.php';               // Nav menu functions goes here
 require_once get_template_directory() . '/lib/sidebars.php';          // Register your sidebars here
 require_once get_template_directory() . '/lib/widgets.php';           // Widget functions goes here
