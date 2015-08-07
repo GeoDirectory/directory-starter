88304303ca819356a511a9b5cbbf4daa52e3d5c0
Giri <giridharanbtech@gmail.com>
2015-08-07 21:01:06 +0530

    added comment rating styles and tab styles

--- lib/gd.php
+++ lib/gd.php
@@ -41,3 +41,71 @@ function dt_comment_avatar_size() {
 	return 60;
 }
 add_action('geodir_comment_avatar_size', 'dt_comment_avatar_size');
+
+// reviews
+function dt_geodir_comment($comment, $args, $depth)
+{
+	$GLOBALS['comment'] = $comment;
+	switch ($comment->comment_type) :
+		case 'pingback' :
+		case 'trackback' :
+			// Display trackbacks differently than normal comments.
+			?>
+			<li <?php comment_class('geodir-comment'); ?> id="comment-<?php comment_ID(); ?>">
+			<p><?php _e('Pingback:', GEODIRECTORY_TEXTDOMAIN); ?> <?php comment_author_link(); ?> <?php edit_comment_link(__('(Edit)', GEODIRECTORY_TEXTDOMAIN), '<span class="edit-link">', '</span>'); ?></p>
+			<?php
+			break;
+		default :
+			// Proceed with normal comments.
+			global $post;
+			?>
+		<li <?php comment_class('geodir-comment'); ?> id="li-comment-<?php comment_ID(); ?>">
+			<article id="comment-<?php comment_ID(); ?>" class="comment hreview">
+				<header class="comment-meta comment-author vcard">
+					<?php
+					/**
+					 * Filter to modify comment avatar size
+					 *
+					 * You can use this filter to change comment avatar size.
+					 *
+					 * @since 1.0.0
+					 * @package GeoDirectory
+					 */
+					$avatar_size = apply_filters('geodir_comment_avatar_size', 44);
+					echo get_avatar($comment, $avatar_size);
+					printf('<cite><b class="reviewer">%1$s</b> %2$s</cite>',
+						get_comment_author_link(),
+						// If current post author is also comment author, make it known visually.
+						($comment->user_id === $post->post_author) ? '<span>' . __('Post author', GEODIRECTORY_TEXTDOMAIN) . '</span>' : ''
+					);
+					echo "<span class='item'><small><span class='fn'>$post->post_title</span></small></span>";
+					printf('<a href="%1$s"><time datetime="%2$s" class="dtreviewed">%3$s<span class="value-title" title="%2$s"></span></time></a>',
+						esc_url(get_comment_link($comment->comment_ID)),
+						get_comment_time('c'),
+						/* translators: 1: date, 2: time */
+						sprintf(__('%1$s at %2$s', GEODIRECTORY_TEXTDOMAIN), get_comment_date(), get_comment_time())
+					);
+					?>
+				</header>
+				<!-- .comment-meta -->
+
+				<?php if ('0' == $comment->comment_approved) : ?>
+					<p class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', GEODIRECTORY_TEXTDOMAIN); ?></p>
+				<?php endif; ?>
+
+				<section class="comment-content comment">
+					<?php comment_text(); ?>
+				</section>
+				<!-- .comment-content -->
+
+				<div class="reply">
+					<?php edit_comment_link(__('Edit', GEODIRECTORY_TEXTDOMAIN), '<p class="edit-link">', '</p>'); ?>
+					<?php comment_reply_link(array_merge($args, array('reply_text' => __('Reply', GEODIRECTORY_TEXTDOMAIN), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
+				</div>
+				<!-- .reply -->
+			</article>
+			<!-- #comment-## -->
+			<?php
+			break;
+	endswitch; // end comment_type check
+}
