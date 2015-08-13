619c460d6baa5fd36e872c608821d43d28ded118
Giri <giridharanbtech@gmail.com>
2015-08-13 21:55:41 +0530

    Customizer Settings Added

--- header.php
+++ header.php
@@ -20,7 +20,6 @@
 			<?php else : ?>
 				<hgroup>
 					<h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
-					<h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
 				</hgroup>
 			<?php endif; ?>
 		</div>
