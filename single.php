07e78a3758a61307270bfbbf97f6cefdd9ed2035
Giri Dhar <giridharanbtech@gmail.com>
2015-12-15 12:51:13 +0530

    next previous links added

--- single.php
+++ single.php
@@ -19,6 +19,16 @@
 				comments_template();
 			endif;
 
+			// Previous/next post navigation.
+			the_post_navigation( array(
+					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+							'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+							'<span class="post-title">%title</span>',
+					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+							'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+							'<span class="post-title">%title</span>',
+			) );
+
 			// End the loop.
 		endwhile;
 		?>
